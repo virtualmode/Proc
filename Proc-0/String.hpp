d30bc0c32bb36298940777431623197c009219ce
virtualmode <virtualmode@users.noreply.github.com>
2021-05-23 23:21:34 +0700

    Add string class

---
+++ Proc-0/String.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef STRING_HPP
+#define STRING_HPP
+
+#include "Temp/Dependency.h"
+
+#include "Char.hpp"
+#include "CharReader.hpp"
+#include "CharWriter.hpp"
+#include "CharSeeker.hpp"
+
+
+
+#endif // STRING_HPP
