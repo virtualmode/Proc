395b9ca81fc05b22bc3f4f67c3ef25ef5d6d9dac
virtualmode <virtualmode@users.noreply.github.com>
2021-10-02 23:31:31 +0700

    Add Roslyn example

---
+++ bin/roslyn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n < 2)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, Fibonacci! {Fibonacci(8)}");
+        }
+    }
+}
